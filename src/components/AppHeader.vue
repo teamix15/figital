825fe481903ac108d848154b69394030e646178d
Timofei Novik <teamix15@gmail.com>
2025-01-23 12:20:50 +0100

    Add home page view

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="flex justify-between rounded mb-[32px]">
+        <CommonButton label="" class="w-[162px] bg-[#E6007E]" />
+        <div class="flex items-center gap-[20px]">
+            <CommonButton label="search" />
+            <CommonButton label="units" />
+            <CommonButton label="about us" />
+            <div label="" class="w-[41px] h-[41px] rounded-full bg-[#E6007E]" />
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import CommonButton from '@/components/CommonButton.vue';
+</script>
---
+++ src/components/CommonButton.vue
@@ -0,0 +1,14 @@
+<template>
+    <button
+        class="flex items-center content-center uppercase h-[54px] border-3 border-[#E6007E] text-[20px] color-[#111111] rounded-[40px] p-4">
+        {{ label }}
+    </button>
+</template>
+
+<script setup lang="ts">
+interface Props {
+    label: string;
+}
+
+defineProps<Props>()
+</script>
