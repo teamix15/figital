fd683e37303ab7a3863b6d1afd4b49185062244a
teamix15 <teamix15@gmail.com>
2025-06-22 11:33:24 +0300

    add eye icon on password inputs

--- src/views/SignUpView/components/SignUpForm.vue
+++ src/views/SignUpView/components/SignUpForm.vue
@@ -48,14 +48,24 @@
 
           <div>
             <label for="password" class="block text-sm font-medium text-gray-700">PASSWORD</label>
-            <input
-              id="password"
-              v-model="form.password"
-              type="password"
-              required
-              minlength="8"
-              class="mt-1 block w-full border-3 border-primary rounded-[40px] py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-            />
+            <div class="relative">
+              <input
+                id="password"
+                v-model="form.password"
+                :type="showPassword ? 'text' : 'password'"
+                required
+                minlength="8"
+                class="mt-1 block w-full border-3 border-primary rounded-[40px] py-2 px-3 pr-10 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+              />
+              <button type="button" @click="showPassword = !showPassword" class="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400 focus:outline-none cursor-pointer">
+                <span v-if="showPassword">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.875 18.825A10.05 10.05 0 0112 19c-5.523 0-10-5.373-10-7s4.477-7 10-7c1.326 0 2.597.258 3.762.725M19.07 4.93A9.956 9.956 0 0122 12c0 1.627-4.477 7-10 7a9.956 9.956 0 01-7.07-2.93M15 12a3 3 0 11-6 0 3 3 0 016 0z" /><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3l18 18" /></svg>
+                </span>
+                <span v-else>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" /><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" /></svg>
+                </span>
+              </button>
+            </div>
           </div>
         </div>
 
@@ -91,6 +101,8 @@ const form = reactive({
   email: '',
   password: '',
 })
+
+const showPassword = ref(false)
 
 const userStore = useUserStore()
 
